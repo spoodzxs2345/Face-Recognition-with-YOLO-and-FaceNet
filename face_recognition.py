4d6fd0a1afe6d75950b9c98cc72c4db780d696dd
Jan Joshua Aus <104749581+spoodzxs2345@users.noreply.github.com>
2024-04-13 18:58:40 +0800

    add time in functionality

--- face_recognition.py
+++ face_recognition.py
@@ -2,6 +2,8 @@ from deepface import DeepFace
 from ultralytics import YOLO
 from ultralytics.engine.results import Results
 import cv2
+import datetime
+import csv
 
 # load webcam
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -15,6 +17,17 @@ data = 'C:/Users/Delsie/Desktop/projects/face_recognition_v2/data'
 
 # load the model
 model = YOLO('C:/Users/Delsie/Desktop/projects/face_recognition_v2/yolov8n-face_openvino_model')
+
+now = datetime.datetime.now()
+current_date = now.strftime('%Y-%m-%d')
+
+# create a csv file to store the logs
+with open(f'C:/Users/Delsie/Desktop/projects/face_recognition_v2/{current_date}.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Name', 'Date', 'Time'])
+
+
+counter = 0
 
 
 while True:
@@ -33,22 +46,36 @@ while True:
 
                 detected_objects.append((object_name, (x1, y1, x2, y2)))
         
-        # compare the face to the faces in the dataset
-        for i, (object_name, (x1, y1, x2, y2)) in enumerate(detected_objects):
-            face = frame[y1:y2, x1:x2]
-            name = 'Intruder'
+        if counter % 10 == 0:
+            # compare the face to the faces in the dataset
+            for i, (object_name, (x1, y1, x2, y2)) in enumerate(detected_objects):
+                face = frame[y1:y2, x1:x2]
+                name = 'Intruder'
 
-            result = DeepFace.find(face, data, model_name='Facenet', distance_metric='cosine', enforce_detection=False, threshold=0.3)
+                result = DeepFace.find(face, data, model_name='Facenet', distance_metric='euclidean_l2', enforce_detection=False, threshold=0.9)
 
-            # get the name. draw bounding box and put the name on the frame
-            if result[0].shape[0] != 0:
-                raw_name = result[0]['identity'][0].split('/')[-1]
-                name = raw_name.split('\\')[1]
+                # get the name. draw bounding box and put the name on the frame
+                if result[0].shape[0] != 0:
+                    raw_name = result[0]['identity'][0].split('/')[-1]
+                    name = raw_name.split('\\')[1]
 
-            color = (0, 0, 255) if name == 'Intruder' else (0, 255, 0)
+                    # check if the name is already in the csv file
+                    with open(f'C:/Users/Delsie/Desktop/projects/face_recognition_v2/{current_date}.csv', 'r') as file:
+                        reader = csv.reader(file)
+                        next(reader)
+                        names = [row[0] for row in reader]
+                    
+                    if name not in names:
+                        with open(f'C:/Users/Delsie/Desktop/projects/face_recognition_v2/{current_date}.csv', 'a', newline='') as file:
+                            writer = csv.writer(file)
+                            writer.writerow([name, current_date, now.strftime('%H:%M:%S')])
 
-            cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
-            cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+                color = (0, 0, 255) if name == 'Intruder' else (0, 255, 0)
+
+                cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+                cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+
+        counter += 1
 
         cv2.imshow('feed', frame)
 
